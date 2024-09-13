cd9a42ad1a1333b2185fc079321b26db7ce8b2a8
林家弘 <82699511+Garvin2009@users.noreply.github.com>
2024-09-13 22:34:57 +0800

    Add files via upload

---
+++ script_ntut/info.py
@@ -0,0 +1,8 @@
+TOTAL_PAGES = 23
+# personal information below
+ID = "學號：113598097 "  # student ID here
+NAME = "姓名：林家弘"  # name here
+
+# ID = "   "  # student ID here
+# NAME = "  "  # name here
+NUMBER = 1  # number here
